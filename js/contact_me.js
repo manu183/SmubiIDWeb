862caf5734091af4411b45ebe2935e7237f35cbf
manu183 <manuellang183@gmail.com>
2018-06-08 02:27:54 +0200

    attempt to fix contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/manuellang183@gmail.com",
+                url: "https://formspree.io/manuellang183@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
